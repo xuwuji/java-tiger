ce21ff632b249baca45406ab106d461b208fefe7
craig.xu <craig.xu@sap.com>
2019-04-24 00:11:56 +0800

    fix

--- java-common/backend/src/main/java/com/xuwuji/eshop/util/TokenUtil.java
+++ java-common/backend/src/main/java/com/xuwuji/eshop/util/TokenUtil.java
@@ -14,7 +14,7 @@ public class TokenUtil {
 	// 微信统一下单接口地址
 	public static final String PAY_URL = "https://api.mch.weixin.qq.com/pay/unifiedorder";
 	// 微信统一下单回调地址
-	public static final String NOTIFY_URL = "http://xuwuji.ngrok.xiaomiqiu.cn/backend/pay/wxNotify";
+	public static final String NOTIFY_URL = "https://jawiv.cn/backend/pay/wxNotify";
 	// 微信查询订单接口地址
 	public static final String ORDER_QUERY_URL = "https://api.mch.weixin.qq.com/pay/orderquery";
 	
