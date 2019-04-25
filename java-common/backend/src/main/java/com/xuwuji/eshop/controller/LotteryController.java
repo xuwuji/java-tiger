da06da56351247d7855d925b310000e452445d1f
xuwuji <craigxu@foxmail.com>
2019-04-25 18:22:28 +0800

    init lottery

---
+++ java-common/backend/src/main/java/com/xuwuji/eshop/controller/LotteryController.java
@@ -0,0 +1,19 @@
+package com.xuwuji.eshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 
+ * 抽奖Controller
+ * 
+ * @author xuwuji
+ *
+ */
+@Controller
+@RequestMapping(value = "/lottery")
+public class LotteryController {
+	
+	
+
+}
