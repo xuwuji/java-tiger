e3a5df167683bb5385bb96308f1fe7a4be1181fd
xuwuji <craigxu@foxmail.com>
2019-07-12 15:04:22 +0800

    upgrade to 1.8

--- java-common/backend/src/main/java/com/xuwuji/eshop/util/ProductUtil.java
+++ java-common/backend/src/main/java/com/xuwuji/eshop/util/ProductUtil.java
@@ -1,7 +1,6 @@
 package com.xuwuji.eshop.util;
 
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 import org.springframework.stereotype.Component;
@@ -12,30 +11,21 @@ import com.xuwuji.eshop.model.SortEnum;
 @Component
 public class ProductUtil {
 
-	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public List<Product> sort(List<Product> products, SortEnum sortRequset) {
 		if (sortRequset == SortEnum.ALL) {
+			return products;
 		} else if (sortRequset == SortEnum.TIME) {
-			Comparator c = new Comparator<Product>() {
-				public int compare(Product p1, Product p2) {
-					return Integer.valueOf(p1.getId()).compareTo(p2.getId());
-				}
-			};
-			products.sort(c);
+			Collections.sort(products, (p1, p2) -> {
+				return Integer.valueOf(p1.getId()).compareTo(p2.getId());
+			});
 		} else if (sortRequset == SortEnum.PRICE) {
-			Comparator c = new Comparator<Product>() {
-				public int compare(Product p1, Product p2) {
-					return Double.valueOf(p1.getPrice()).compareTo(p2.getPrice());
-				}
-			};
-			products.sort(c);
+			Collections.sort(products, (p1, p2) -> {
+				return Double.valueOf(p1.getPrice()).compareTo(p2.getPrice());
+			});
 		} else if (sortRequset == SortEnum.SALE) {
-			Comparator c = new Comparator<Product>() {
-				public int compare(Product p1, Product p2) {
-					return Integer.valueOf(p1.getSalesCount()).compareTo(p2.getSalesCount());
-				}
-			};
-			products.sort(c);
+			Collections.sort(products, (p1, p2) -> {
+				return Integer.valueOf(p1.getSalesCount()).compareTo(p2.getSalesCount());
+			});
 		}
 		Collections.reverse(products);
 		return products;
